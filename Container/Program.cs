8b844e97973e7e3b01f093a4cba8b15732d50323
aronkalo <kalo.aron@outlook.hu>
2021-02-26 19:59:05 +0100

    feat: IoC container and appsettings.json added

--- Container/Program.cs
+++ Container/Program.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Container
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IConfigurationRoot _configuration;
+
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            try
+            {
+                MainAsync(args).Wait();
+                return 0;
+            }
+            catch
+            {
+
+                return 1;
+            }
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Log.Information("Creating service collection");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            Log.Information("Building service provider");
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            try
+            {
+                Log.Information("Starting service");
+                await serviceProvider.GetService<App>().RunAsync();
+                Log.Information("Ending service");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error running App service");
+                throw;
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton(LoggerFactory.Create(builder =>
+            {
+            }));
+
+            serviceCollection.AddLogging();
+
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            //Register dependencies to the IoC container
+            serviceCollection.AddSingleton<IConfiguration>(_configuration);
+            serviceCollection.AddTransient<App>();
         }
     }
 }
